beyond-all-reason/spring

Experimental changes to Spin/Turn animation code. It's mostly here to test if rotation related desync are affected. Should be reverted if they do not.

rts/Sim/Units/Scripts/UnitScript.cpp
@@ -127,14 +127,10 @@ bool CUnitScript::MoveToward(float& cur, float dest, float speed)
  */
 bool CUnitScript::TurnToward(float& cur, float dest, float speed)
 {
-	float delta = dest - cur;
+	assert(dest < math::TWOPI);
+	assert(cur  < math::TWOPI);
 
-	// clamp: -pi .. 0 .. +pi (fmod(x,TWOPI) would do the same but is slower due to streflop)
-	if (delta > math::PI) {
-		delta -= math::TWOPI;
-	} else if (delta <= -math::PI) {
-		delta += math::TWOPI;
-	}
+	float delta = math::fmodf(dest - cur + math::THREEPI, math::TWOPI) - math::PI;
 
 	if (math::fabsf(delta) <= speed) {
 		cur = dest;
@@ -410,6 +406,7 @@ void CUnitScript::TurnNow(int piece, int axis, float destination)
 		ShowUnitScriptError("[US::TurnNow] invalid script piece index");
 		return;
 	}
+	destination = math::fmodf(destination, math::TWOPI);
 
 	LocalModelPiece* p = pieces[piece];
 

rts/Sim/Units/Scripts/UnitScript.h
@@ -109,8 +109,8 @@ public:
 	bool Tick(int tickRate);
 	// note: must copy-and-set here (LMP dirty flag, etc)
 	bool TickMoveAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 pos = lmp.GetPosition(); const bool ret = MoveToward(pos[ai.axis], ai.dest, ai.speed / tickRate); lmp.SetPosition(pos); return ret; }
-	bool TickTurnAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); const bool ret = TurnToward(rot[ai.axis], ai.dest, ai.speed / tickRate); lmp.SetRotation(rot); return ret; }
-	bool TickSpinAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); const bool ret = DoSpin(rot[ai.axis], ai.dest, ai.speed, ai.accel, tickRate); lmp.SetRotation(rot); return ret; }
+	bool TickTurnAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); rot[ai.axis] = math::fmodf(rot[ai.axis], math::TWOPI); const bool ret = TurnToward(rot[ai.axis], ai.dest, ai.speed / tickRate         ); lmp.SetRotation(rot); return ret; }
+	bool TickSpinAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); rot[ai.axis] = math::fmodf(rot[ai.axis], math::TWOPI); const bool ret =     DoSpin(rot[ai.axis], ai.dest, ai.speed, ai.accel, tickRate); lmp.SetRotation(rot); return ret; }
 	void TickAnims(int tickRate, const TickAnimFunc& tickAnimFunc, AnimContainerType& liveAnims, AnimContainerType& doneAnims);
 
 	// animation, used by CCobThread

SpliFF/metalstorm

rts/Sim/Units/Scripts/UnitScript.cpp
@@ -127,14 +127,10 @@ bool CUnitScript::MoveToward(float& cur, float dest, float speed)
  */
 bool CUnitScript::TurnToward(float& cur, float dest, float speed)
 {
-	float delta = dest - cur;
+	assert(dest < math::TWOPI);
+	assert(cur  < math::TWOPI);
 
-	// clamp: -pi .. 0 .. +pi (fmod(x,TWOPI) would do the same but is slower due to streflop)
-	if (delta > math::PI) {
-		delta -= math::TWOPI;
-	} else if (delta <= -math::PI) {
-		delta += math::TWOPI;
-	}
+	float delta = math::fmod(dest - cur + math::THREEPI, math::TWOPI) - math::PI;
 
 	if (math::fabsf(delta) <= speed) {
 		cur = dest;
@@ -410,6 +406,7 @@ void CUnitScript::TurnNow(int piece, int axis, float destination)
 		ShowUnitScriptError("[US::TurnNow] invalid script piece index");
 		return;
 	}
+	destination = math::fmod(destination, math::TWOPI);
 
 	LocalModelPiece* p = pieces[piece];
 

rts/Sim/Units/Scripts/UnitScript.h
@@ -109,8 +109,8 @@ public:
 	bool Tick(int tickRate);
 	// note: must copy-and-set here (LMP dirty flag, etc)
 	bool TickMoveAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 pos = lmp.GetPosition(); const bool ret = MoveToward(pos[ai.axis], ai.dest, ai.speed / tickRate); lmp.SetPosition(pos); return ret; }
-	bool TickTurnAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); const bool ret = TurnToward(rot[ai.axis], ai.dest, ai.speed / tickRate); lmp.SetRotation(rot); return ret; }
-	bool TickSpinAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); const bool ret = DoSpin(rot[ai.axis], ai.dest, ai.speed, ai.accel, tickRate); lmp.SetRotation(rot); return ret; }
+	bool TickTurnAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); rot[ai.axis] = math::fmod(rot[ai.axis], math::TWOPI); const bool ret = TurnToward(rot[ai.axis], ai.dest, ai.speed / tickRate         ); lmp.SetRotation(rot); return ret; }
+	bool TickSpinAnim(int tickRate, LocalModelPiece& lmp, AnimInfo& ai) { float3 rot = lmp.GetRotation(); rot[ai.axis] = math::fmod(rot[ai.axis], math::TWOPI); const bool ret =     DoSpin(rot[ai.axis], ai.dest, ai.speed, ai.accel, tickRate); lmp.SetRotation(rot); return ret; }
 	void TickAnims(int tickRate, const TickAnimFunc& tickAnimFunc, AnimContainerType& liveAnims, AnimContainerType& doneAnims);
 
 	// animation, used by CCobThread