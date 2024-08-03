SpliFF/metalstorm

commit 9607eaebdbd39ba089c4467d55beacdf9708c481

Patch up https://github.com/beyond-all-reason/spring/issues/1500 Fix order of SDL2 window setting functions Fix  LuaUnsyncedCtrl::SetWindowGeometry() docs

rts/Rendering/GlobalRendering.cpp
@@ -1165,7 +1165,7 @@ void CGlobalRendering::SetWindowAttributes(SDL_Window* window)
 	      int2 newRes = GetCfgWinRes();
 
 	LOG("[GR::%s][1] cfgFullScreen=%d numDisplays=%d winPos=<%d,%d> newRes=<%d,%d>", __func__, fullScreen, numDisplays, winPosX, winPosY, newRes.x, newRes.y);
-	GetWindowPosSizeBounded(winPosX, winPosY, newRes.x, newRes.y);
+	//GetWindowPosSizeBounded(winPosX, winPosY, newRes.x, newRes.y);
 	LOG("[GR::%s][2] cfgFullScreen=%d numDisplays=%d winPos=<%d,%d> newRes=<%d,%d>", __func__, fullScreen, numDisplays, winPosX, winPosY, newRes.x, newRes.y);
 
 //	if (SDL_SetWindowFullscreen(window, 0) != 0)
@@ -1173,12 +1173,13 @@ void CGlobalRendering::SetWindowAttributes(SDL_Window* window)
 
 	SDL_RestoreWindow(window);
 	SDL_SetWindowMinimumSize(window, minRes.x, minRes.y);
-	SDL_SetWindowPosition(window, winPosX, winPosY);
-	SDL_SetWindowSize(window, newRes.x, newRes.y);
-	SDL_SetWindowBordered(window, borderless ? SDL_FALSE : SDL_TRUE);
 
 	if (SDL_SetWindowFullscreen(window, (borderless ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_FULLSCREEN) * fullScreen) != 0)
 		LOG("[GR::%s][4][SDL_SetWindowFullscreen] err=\"%s\"", __func__, SDL_GetError());
+
+	SDL_SetWindowPosition(window, winPosX, winPosY);
+	SDL_SetWindowSize(window, newRes.x, newRes.y);
+	SDL_SetWindowBordered(window, borderless ? SDL_FALSE : SDL_TRUE);
 
 	if (newRes == maxRes)
 		SDL_MaximizeWindow(window);

rts/Lua/LuaUnsyncedCtrl.cpp
@@ -4827,8 +4827,8 @@ int LuaUnsyncedCtrl::SDLStopTextInput(lua_State* L)
  *
  * @function Spring.SetWindowGeometry
  * @number displayIndex
- * @number winPosX
- * @number winPosY
+ * @number winRelPosX
+ * @number winRelPosY
  * @number winSizeX
  * @number winSizeY
  * @bool fullScreen

commit d58767951881252cef3b69c7882e4f26835421e1

Attempt to rectify Ubuntu fullscreen issue introduced in 9607eaebdbd39ba089c4467d55beacdf9708c481

rts/Rendering/GlobalRendering.cpp
@@ -1174,11 +1174,12 @@ void CGlobalRendering::SetWindowAttributes(SDL_Window* window)
 	SDL_RestoreWindow(window);
 	SDL_SetWindowMinimumSize(window, minRes.x, minRes.y);
 
+	SDL_SetWindowPosition(window, winPosX, winPosY);
+	SDL_SetWindowSize(window, newRes.x, newRes.y);
+
 	if (SDL_SetWindowFullscreen(window, (borderless ? SDL_WINDOW_FULLSCREEN_DESKTOP : SDL_WINDOW_FULLSCREEN) * fullScreen) != 0)
 		LOG("[GR::%s][4][SDL_SetWindowFullscreen] err=\"%s\"", __func__, SDL_GetError());
 
-	SDL_SetWindowPosition(window, winPosX, winPosY);
-	SDL_SetWindowSize(window, newRes.x, newRes.y);
 	SDL_SetWindowBordered(window, borderless ? SDL_FALSE : SDL_TRUE);
 
 	if (newRes == maxRes)