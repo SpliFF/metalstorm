SpliFF/metalstorm

commit 22cb8b62747eb06eb6bfc761f60a8e75fcdb187b

Fix icons bleeding and some atlas calculation inconsistencies

rts/Rendering/Textures/3DOTextureHandler.cpp
@@ -101,7 +101,7 @@ void C3DOTextureHandler::Init()
 
 		const size_t foundx = atlasAlloc->GetEntry(texFile.name).x;
 		const size_t foundy = atlasAlloc->GetEntry(texFile.name).y;
-		const auto texCoords = atlasAlloc->GetTexCoords(texFile.name);
+		const auto texCoords = atlasAlloc->GetTexCoordsCntr(texFile.name);
 
 		const uint8_t* rmem1 = curtex1.GetRawMem();
 		const uint8_t* rmem2 = curtex2.GetRawMem();

rts/Rendering/Textures/TextureRenderAtlas.cpp
@@ -62,6 +62,7 @@ CTextureRenderAtlas::CTextureRenderAtlas(
 	CTextureAtlas::AllocatorType allocType_,
 	int atlasSizeX,
 	int atlasSizeY,
+	int maxLevels,
 	uint32_t glInternalType_,
 	const std::string& atlasName_
 	)
@@ -93,6 +94,7 @@ CTextureRenderAtlas::CTextureRenderAtlas(
 	atlasSizeY = std::min<int>(globalRendering->maxTextureSize, (atlasSizeY > 0) ? atlasSizeY : configHandler->GetInt("MaxTextureAtlasSizeY"));
 
 	atlasAllocator->SetMaxSize(atlasSizeX, atlasSizeY);
+	atlasAllocator->SetMaxTexLevel(maxLevels);
 
 	if (shaderRef == 0) {
 		shader = shaderHandler->CreateProgramObject("[TextureRenderAtlas]", "TextureRenderAtlas");
@@ -227,7 +229,7 @@ AtlasedTexture CTextureRenderAtlas::GetTexture(const std::string& texName)
 	if (it == nameToUniqueSubTexStr.end())
 		return AtlasedTexture::DefaultAtlasTexture;
 
-	return AtlasedTexture(atlasAllocator->GetTexCoords(it->second));
+	return AtlasedTexture(atlasAllocator->GetTexCoordsEdge(it->second));
 }
 
 AtlasedTexture CTextureRenderAtlas::GetTexture(const std::string& texName, const std::string& texBackupName)
@@ -238,7 +240,7 @@ AtlasedTexture CTextureRenderAtlas::GetTexture(const std::string& texName, const
 
 	auto it = nameToUniqueSubTexStr.find(texName);
 	if (it != nameToUniqueSubTexStr.end())
-		return AtlasedTexture(atlasAllocator->GetTexCoords(it->second));
+		return AtlasedTexture(atlasAllocator->GetTexCoordsEdge(it->second));
 
 	if (texBackupName.empty())
 		return AtlasedTexture::DefaultAtlasTexture;
@@ -288,12 +290,6 @@ int CTextureRenderAtlas::GetNumTexLevels() const
 {
 	RECOIL_DETAILED_TRACY_ZONE;
 	return atlasAllocator->GetNumTexLevels();
-}
-
-void CTextureRenderAtlas::SetMaxTexLevel(int maxLevels)
-{
-	RECOIL_DETAILED_TRACY_ZONE;
-	atlasAllocator->SetMaxTexLevel(maxLevels);
 }
 
 bool CTextureRenderAtlas::IsValid() const
@@ -409,7 +405,7 @@ bool CTextureRenderAtlas::CreateAtlasTexture()
 
 			for (uint32_t page = 0; page < numPages; ++page) {
 				for (uint32_t level = 0; level < numLevels; ++level) {
-					glViewport(0, 0, std::max(atlasSize.x >> level, 1), std::max(atlasSize.y >> level, 1));
+					glViewport(0, 0, std::max(atlasSize.x >> level, 1u), std::max(atlasSize.y >> level, 1u));
 
 					if (numPages > 1)
 						fbo.AttachTextureLayer(atlasTex->GetId(), GL_COLOR_ATTACHMENT0, level, page);
@@ -426,7 +422,7 @@ bool CTextureRenderAtlas::CreateAtlasTexture()
 						if (entry.texCoords.pageNum != page)
 							continue;
 
-						const auto atlasedTexCoords = atlasAllocator->GetTexCoords(uniqTexName);
+						const auto atlasedTexCoords = atlasAllocator->GetTexCoordsEdge(uniqTexName);
 						const auto& [srcTexID, srcSubTC] = uniqueSubTextureMap[uniqTexName];
 
 						if (srcTexID == 0)

rts/Rendering/Textures/TextureRenderAtlas.h
@@ -27,7 +27,7 @@ public:
 		bool operator==(const UniqueSubTexture&) const = default;
 	};
 public:
-	CTextureRenderAtlas(CTextureAtlas::AllocatorType allocType, int atlasSizeX, int atlasSizeY, uint32_t glInternalType = /*GL_RGBA8*/0x8058, const std::string& atlasName = "");
+	CTextureRenderAtlas(CTextureAtlas::AllocatorType allocType, int atlasSizeX, int atlasSizeY, int maxLevels, uint32_t glInternalType = /*GL_RGBA8*/0x8058, const std::string& atlasName = "");
 	~CTextureRenderAtlas();
 	CTextureRenderAtlas(const CTextureRenderAtlas&) = delete;
 	CTextureRenderAtlas(CTextureRenderAtlas&&) noexcept = default;
@@ -54,7 +54,6 @@ public:
 	const int2& GetAtlasSize() const;
 	int GetMinDim() const;
 	int GetNumTexLevels() const;
-	void SetMaxTexLevel(int maxLevels);
 
 	const IAtlasAllocator* GetAllocator() const { return atlasAllocator.get(); }
 	const std::string& GetAtlasName() const { return atlasName; }

rts/Rendering/Env/Decals/GroundDecalHandler.cpp
@@ -414,12 +414,10 @@ uint32_t CGroundDecalHandler::GetDepthBufferTextureTarget() const
 	return highQuality ? GL_TEXTURE_2D_MULTISAMPLE : GL_TEXTURE_2D;
 }
 
-static constexpr CTextureAtlas::AllocatorType defAllocType = CTextureAtlas::ATLAS_ALLOC_MP_LEGACY;
-static constexpr int defNumLevels = 4;
+static constexpr auto DEFAULT_ALLOC_TYPE = CTextureAtlas::ATLAS_ALLOC_MP_LEGACY;
+static constexpr auto DEFAULT_NUM_OF_TEXTURE_LEVELS = 4;
 void CGroundDecalHandler::GenerateAtlasTexture() {
-	atlasTex = std::make_unique<CTextureRenderAtlas>(defAllocType, 0, 0, GL_RGBA8, "Decals");
-
-	atlasTex->SetMaxTexLevel(defNumLevels);
+	atlasTex = std::make_unique<CTextureRenderAtlas>(DEFAULT_ALLOC_TYPE, 0, 0, DEFAULT_NUM_OF_TEXTURE_LEVELS, GL_RGBA8, "Decals");
 
 	// often represented by compressed textures, cannot be added to the regular atlas
 	AddBuildingDecalTextures();

rts/Rendering/IconHandler.cpp
@@ -8,6 +8,8 @@
 #include <cctype>
 #include <cmath>
 
+#include <fmt/format.h>
+
 #include "Rendering/GL/myGL.h"
 #include "Rendering/GL/RenderBuffers.h"
 #include "System/Log/ILog.h"
@@ -46,10 +48,16 @@ void CIconHandler::Kill()
 
 void CIconHandler::DumpAtlasTextures() const
 {
-	if (atlasTextureIDs[0])
-		glSaveTexture(atlasTextureIDs[0], "IconsAtlas1.png");
-	if (atlasTextureIDs[1])
-		glSaveTexture(atlasTextureIDs[1], "IconsAtlas2.png");
+	if (atlasTextureIDs[0]) {
+		for (int level = 0; level < DEFAULT_NUM_OF_TEXTURE_LEVELS; ++level) {
+			glSaveTexture(atlasTextureIDs[0], fmt::format("IconsAtlas1-{}.png", level).c_str(), level);
+		}
+	}
+	if (atlasTextureIDs[1]) {
+		for (int level = 0; level < DEFAULT_NUM_OF_TEXTURE_LEVELS; ++level) {
+			glSaveTexture(atlasTextureIDs[1], fmt::format("IconsAtlas2-{}.png", level).c_str(), level);
+		}
+	}
 }
 
 bool CIconHandler::UpdateAtlasData(size_t atlasIdx)
@@ -58,7 +66,7 @@ bool CIconHandler::UpdateAtlasData(size_t atlasIdx)
 	if (atlas)
 		return true;
 
-	atlas = std::make_unique<CTextureRenderAtlas>(CTextureAtlas::ATLAS_ALLOC_LEGACY, 0, 0, GL_RGBA8, IntToString(atlasIdx, "IconsAtlas_%i"));
+	atlas = std::make_unique<CTextureRenderAtlas>(CTextureAtlas::ATLAS_ALLOC_LEGACY, 0, 0, DEFAULT_NUM_OF_TEXTURE_LEVELS, GL_RGBA8, IntToString(atlasIdx, "IconsAtlas_%i"));
 
 	spring::unordered_set<std::string> invalidIcons;
 	for (const auto& [iconName, iconIndex] : iconsMap) {
@@ -130,7 +138,6 @@ bool CIconHandler::CreateAtlasTexture(size_t atlasIdx)
 		return true;
 	}
 
-	atlas->SetMaxTexLevel(DEFAULT_NUM_OF_TEXTURE_LEVELS);
 	if (!atlas->CreateAtlasTexture())
 		return false;
 

rts/Rendering/Textures/IAtlasAllocator.h
@@ -33,7 +33,7 @@ public:
 	IAtlasAllocator() = default;
 	virtual ~IAtlasAllocator() {}
 
-	void SetMaxSize(int xsize, int ysize) { maxsize = int2(xsize, ysize); }
+	void SetMaxSize(uint32_t xsize, uint32_t ysize) { maxsize = uint2(xsize, ysize); }
 public:
 	virtual bool Allocate() = 0;
 	virtual int GetNumTexLevels() const = 0;
@@ -46,6 +46,17 @@ public:
 	{
 		minDim = argmin(minDim, size.x, size.y);
 		entries[name] = SAtlasEntry(size, name);
+	}
+
+	inline int GetPadding() const
+	{
+		return 1 << (numLevels - 1);
+	}
+
+	void SizeRoundUp()
+	{
+		atlasSize.x = std::min(AlignUp(atlasSize.x, GetPadding()), maxsize.x);
+		atlasSize.y = std::min(AlignUp(atlasSize.y, GetPadding()), maxsize.y);
 	}
 
 	auto FindEntry(const std::string& name) const
@@ -62,29 +73,56 @@ public:
 	const auto& GetEntry(const std::string& name) const { return GetEntry(FindEntry(name));	}
 	const auto& GetEntries() const { return entries; }
 
-	AtlasedTexture GetTexCoords(const spring::unordered_map<std::string, SAtlasEntry>::const_iterator& it)
+	// pixel center based UV
+	AtlasedTexture GetTexCoordsCntr(const spring::unordered_map<std::string, SAtlasEntry>::const_iterator& it)
 	{
 		if (it == entries.end())
 			return AtlasedTexture::DefaultAtlasTexture;
 
-		AtlasedTexture uv = it->second.texCoords;
+		const AtlasedTexture& a = it->second.texCoords; // all coords are inclusive
+		AtlasedTexture uv = a;
 
-		uv.x1 /= atlasSize.x;
-		uv.y1 /= atlasSize.y;
-		uv.x2 /= atlasSize.x;
-		uv.y2 /= atlasSize.y;
+		const float invW = 1.0f / atlasSize.x;
+		const float invH = 1.0f / atlasSize.y;
 
-		// adjust texture coordinates by half a texel (opengl uses centeroids)
-		uv.x1 += 0.5f / atlasSize.x;
-		uv.y1 += 0.5f / atlasSize.y;
-		uv.x2 += 0.5f / atlasSize.x;
-		uv.y2 += 0.5f / atlasSize.y;
+		// Convert inclusive texel indices -> normalized coordinates of texel centers.
+		uv.x1 = (a.x1 + 0.5f) * invW;
+		uv.y1 = (a.y1 + 0.5f) * invH;
+		uv.x2 = (a.x2 + 0.5f) * invW;
+		uv.y2 = (a.y2 + 0.5f) * invH;
 
 		return uv;
 	}
-	AtlasedTexture GetTexCoords(const std::string& name)
+
+	// pixel edges based UV
+	AtlasedTexture GetTexCoordsEdge(const spring::unordered_map<std::string, SAtlasEntry>::const_iterator& it) const
 	{
-		return GetTexCoords(FindEntry(name));
+		if (it == entries.end())
+			return AtlasedTexture::DefaultAtlasTexture;
+
+		const AtlasedTexture& a = it->second.texCoords; // all coords are inclusive
+		AtlasedTexture uv = a;
+
+		const float invW = 1.0f / atlasSize.x;
+		const float invH = 1.0f / atlasSize.y;
+
+		// Edges in texel space are [x1, x2+1] and [y1, y2+1] because x2/y2 are inclusive.
+		uv.x1 = (a.x1       ) * invW;
+		uv.y1 = (a.y1       ) * invH;
+		uv.x2 = (a.x2 + 1.0f) * invW;
+		uv.y2 = (a.y2 + 1.0f) * invH;
+
+		return uv;
+	}
+
+	AtlasedTexture GetTexCoordsCntr(const std::string& name)
+	{
+		return GetTexCoordsCntr(FindEntry(name));
+	}
+
+	AtlasedTexture GetTexCoordsEdge(const std::string& name)
+	{
+		return GetTexCoordsEdge(FindEntry(name));
 	}
 
 	bool contains(const std::string& name) const
@@ -106,8 +144,8 @@ public:
 protected:
 	spring::unordered_map<std::string, SAtlasEntry> entries;
 
-	int2 atlasSize;
-	int2 maxsize = {2048, 2048};
+	uint2 atlasSize;
+	uint2 maxsize = {2048, 2048};
 	int numLevels = std::numeric_limits<int>::max();
 	int minDim = std::numeric_limits<int>::max();
 };

rts/Rendering/Textures/LegacyAtlasAlloc.cpp
@@ -81,7 +81,7 @@ bool CLegacyAtlasAlloc::Allocate()
 	std::list<int2> nextSub;
 	std::list<int2> thisSub;
 
-	int padding = 1 << GetNumTexLevels();
+	int padding = GetPadding();
 
 	for (int a = 0; a < static_cast<int>(memtextures.size()); ++a) {
 		SAtlasEntry* curtex = memtextures[a];
@@ -159,6 +159,7 @@ bool CLegacyAtlasAlloc::Allocate()
 	}
 
 	atlasSize = max;
+	SizeRoundUp();
 
 	return success;
 }

rts/Rendering/Textures/MultiPageAtlasAlloc.hpp
@@ -130,7 +130,7 @@ public:
 
 		return (reqLevels == std::numeric_limits<int>::max()) ? 0 : reqLevels;
 	}
-	uint32_t GetNumPages() const override { return allocators.size(); }
+	uint32_t GetNumPages() const override { return static_cast<uint32_t>(allocators.size()); }
 private:
 	uint32_t maxPages;
 	std::vector<std::unique_ptr<AtlasAlloc>> allocators;

commit d615251dea863f236bda67c8cbafab15a1d940f8

Fix inadvertent return of a reference to a temporary value caused crashes on Linux (introduced in https://github.com/beyond-all-reason/RecoilEngine/commit/22cb8b62747eb06eb6bfc761f60a8e75fcdb187b). Also some int --> uint.

rts/Rendering/Textures/3DOTextureHandler.cpp
@@ -58,14 +58,14 @@ void C3DOTextureHandler::Init()
 	}
 
 	const bool allocated = atlasAlloc->Allocate();
-	const int2 curAtlasSize = atlasAlloc->GetAtlasSize();
-	const int2 maxAtlasSize = atlasAlloc->GetMaxSize();
+	const auto& curAtlasSize = atlasAlloc->GetAtlasSize();
+	const auto& maxAtlasSize = atlasAlloc->GetMaxSize();
 
 	if (!allocated) {
 		// either the algorithm simply failed to fit all
 		// textures or the textures would really not fit
 		LOG_L(L_WARNING,
-			"[%s] failed to allocate 3DO texture-atlas memory (size=%dx%d max=%dx%d)",
+			"[%s] failed to allocate 3DO texture-atlas memory (size=%ux%u max=%ux%u)",
 			__func__,
 			curAtlasSize.x, curAtlasSize.y,
 			maxAtlasSize.x, maxAtlasSize.y

rts/Rendering/Textures/Texture.cpp
@@ -119,7 +119,7 @@ namespace GL {
 		return *this;
 	}
 
-	Texture2D::Texture2D(int xsize_, int ysize_, uint32_t intFormat_, const TextureCreationParams& tcp, bool wantCompress)
+	Texture2D::Texture2D(uint32_t xsize_, uint32_t ysize_, uint32_t intFormat_, const TextureCreationParams& tcp, bool wantCompress)
 		: Texture2D()
 	{
 		size = int2(xsize_, ysize_);
@@ -183,7 +183,7 @@ namespace GL {
 	}
 
 
-	Texture2DArray::Texture2DArray(int xsize_, int ysize_, int numPages_, uint32_t intFormat_, const TextureCreationParams& tcp, bool wantCompress)
+	Texture2DArray::Texture2DArray(uint32_t xsize_, uint32_t ysize_, uint32_t numPages_, uint32_t intFormat_, const TextureCreationParams& tcp, bool wantCompress)
 		: Texture2DArray()
 	{
 		size = int2(xsize_, ysize_);

rts/Rendering/Textures/Texture.hpp
@@ -51,8 +51,8 @@ namespace GL {
 		Texture2D()
 			: TextureBase(0x0DE1/*GL_TEXTURE_2D*/)
 		{}
-		Texture2D(int xsize_, int ysize_, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true);
-		Texture2D(const int2& size, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true)
+		Texture2D(uint32_t xsize_, uint32_t ysize_, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true);
+		Texture2D(const uint2& size, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true)
 			: Texture2D(size.x, size.y, intFormat_, tcp_, wantCompress)
 		{}
 
@@ -105,8 +105,8 @@ namespace GL {
 			: TextureBase(0x8C1A/*GL_TEXTURE_2D_ARRAY*/)
 			, numPages(0)
 		{}
-		Texture2DArray(int xsize_, int ysize_, int numPages_, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true);
-		Texture2DArray(const int2& size, int numPages_, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true)
+		Texture2DArray(uint32_t xsize_, uint32_t ysize_, uint32_t numPages_, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true);
+		Texture2DArray(const uint2& size, uint32_t numPages_, uint32_t intFormat_, const TextureCreationParams& tcp_, bool wantCompress = true)
 			: Texture2DArray(size.x, size.y, numPages_, intFormat_, tcp_, wantCompress)
 		{}
 
@@ -152,6 +152,6 @@ namespace GL {
 		auto GetNumPages() const { return numPages; }
 	private:
 		int2 size;
-		int numPages;
+		uint32_t numPages;
 	};
 }

rts/Rendering/Textures/TextureRenderAtlas.cpp
@@ -280,7 +280,7 @@ int CTextureRenderAtlas::GetMinDim() const
 	return atlasAllocator->GetMinDim();
 }
 
-const int2& CTextureRenderAtlas::GetAtlasSize() const
+const uint2& CTextureRenderAtlas::GetAtlasSize() const
 {
 	RECOIL_DETAILED_TRACY_ZONE;
 	return atlasAllocator->GetAtlasSize();
@@ -358,7 +358,7 @@ bool CTextureRenderAtlas::CreateAtlasTexture()
 	const auto numLevels = atlasAllocator->GetNumTexLevels();
 	const auto numPages = atlasAllocator->GetNumPages();
 
-	const auto atlasSize = atlasAllocator->GetAtlasSize();
+	const auto& atlasSize = atlasAllocator->GetAtlasSize();
 
 	{
 		GL::TextureCreationParams tcp{

rts/Rendering/Textures/TextureRenderAtlas.h
@@ -51,7 +51,7 @@ public:
 
 	uint32_t GetTexTarget() const;
 	uint32_t GetTexID() const;
-	const int2& GetAtlasSize() const;
+	const uint2& GetAtlasSize() const;
 	int GetMinDim() const;
 	int GetNumTexLevels() const;
 